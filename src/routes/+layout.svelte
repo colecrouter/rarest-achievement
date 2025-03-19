14b7c3ce1abe245cdb36d8cc244d17de3183bb6f
Cole Crouter <colecrouter1@gmail.com>
2025-03-19 22:20:25 +0000

    Added skeleton UI with Tailwind CSS

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-import { i18n } from "$lib/i18n";
-import { ParaglideJS } from "@inlang/paraglide-sveltekit";
-const { children } = $props();
+    import "../app.css";
+    import { i18n } from "$lib/i18n";
+    import { ParaglideJS } from "@inlang/paraglide-sveltekit";
+    const { children } = $props();
 </script>
 
 <ParaglideJS {i18n}>

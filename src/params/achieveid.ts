5a9eb4f97c2d08a69c39b5ee0bbd60bf1c346cf2
Cole Crouter <colecrouter1@gmail.com>
2025-04-09 19:43:20 -0600

    Allowed achievement ID to contain lowercase letters

--- src/params/achieveid.ts
+++ src/params/achieveid.ts
@@ -3,5 +3,5 @@ import type { ParamMatcher } from "@sveltejs/kit";
 // Match the Steam achievement ID format (TF_SCOUT_ACHIEVE_PROGRESS1)
 
 export const match: ParamMatcher = (param) => {
-    return /^[A-Z0-9_]+$/.test(param);
+    return /^[a-zA-Z0-9_]+$/.test(param);
 };

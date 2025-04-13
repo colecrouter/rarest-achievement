30f88c569d9e0e0d0e840a3c1a93ed9cc0d0edec
Cole Crouter <colecrouter1@gmail.com>
2025-04-13 16:17:11 -0600

    New branding #3

--- src/routes/(app)/Navbar.svelte
+++ src/routes/(app)/Navbar.svelte
@@ -15,62 +15,8 @@
     >
         <!-- Logo and title -->
         <a href="/" class="flex items-center gap-2">
-            <div class="relative h-6 w-6">
-                <!-- Vault-->
-                <svg
-                    class="absolute h-6 w-6 text-amber-500"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="48"
-                    height="48"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    ><rect width="18" height="18" x="3" y="3" rx="2"
-                    ></rect><circle cx="7.5" cy="7.5" r=".5" fill="currentColor"
-                    ></circle><path d="m7.9 7.9 2.7 2.7"></path><circle
-                        cx="16.5"
-                        cy="7.5"
-                        r=".5"
-                        fill="currentColor"
-                    ></circle><path d="m13.4 10.6 2.7-2.7"></path><circle
-                        cx="7.5"
-                        cy="16.5"
-                        r=".5"
-                        fill="currentColor"
-                    ></circle><path d="m7.9 16.1 2.7-2.7"></path><circle
-                        cx="16.5"
-                        cy="16.5"
-                        r=".5"
-                        fill="currentColor"
-                    ></circle><path d="m13.4 13.4 2.7 2.7"></path><circle
-                        cx="12"
-                        cy="12"
-                        r="2"
-                    ></circle>
-                </svg>
-
-                <!-- Smoke -->
-                <svg
-                    class="absolute -top-5.5 text-gray-300"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                >
-                    <path d="M11 21c0-2.5 2-2.5 2-5"></path>
-                    <path d="M16 21c0-2.5 2-2.5 2-5"></path>
-                    <path d="M6 21c0-2.5 2-2.5 2-5"></path>
-                </svg>
-            </div>
-            <h1 class="text-xl font-bold">Steam Vault</h1>
+            <div class="logo bg-primary-500"></div>
+            <h1 class="hidden text-xl font-bold md:block">Steam Vault</h1>
         </a>
 
         <!-- Search form -->
@@ -101,6 +47,10 @@
                     class="btn btn-sm preset-filled-primary-500"
                     type="submit"
                 >
+                    <div>
+                        Login
+                        <span class="hidden md:inline"> with Steam </span>
+                    </div>
                     <svg
                         role="img"
                         viewBox="0 0 24 24"
@@ -114,9 +64,18 @@
                             d="M11.979 0C5.678 0 .511 4.86.022 11.037l6.432 2.658c.545-.371 1.203-.59 1.912-.59.063 0 .125.004.188.006l2.861-4.142V8.91c0-2.495 2.028-4.524 4.524-4.524 2.494 0 4.524 2.031 4.524 4.527s-2.03 4.525-4.524 4.525h-.105l-4.076 2.911c0 .052.004.105.004.159 0 1.875-1.515 3.396-3.39 3.396-1.635 0-3.016-1.173-3.331-2.727L.436 15.27C1.862 20.307 6.486 24 11.979 24c6.627 0 11.999-5.373 11.999-12S18.605 0 11.979 0zM7.54 18.21l-1.473-.61c.262.543.714.999 1.314 1.25 1.297.539 2.793-.076 3.332-1.375.263-.63.264-1.319.005-1.949s-.75-1.121-1.377-1.383c-.624-.26-1.29-.249-1.878-.03l1.523.63c.956.4 1.409 1.5 1.009 2.455-.397.957-1.497 1.41-2.454 1.012H7.54zm11.415-9.303c0-1.662-1.353-3.015-3.015-3.015-1.665 0-3.015 1.353-3.015 3.015 0 1.665 1.35 3.015 3.015 3.015 1.663 0 3.015-1.35 3.015-3.015zm-5.273-.005c0-1.252 1.013-2.266 2.265-2.266 1.249 0 2.266 1.014 2.266 2.266 0 1.251-1.017 2.265-2.266 2.265-1.253 0-2.265-1.014-2.265-2.265z"
                         />
                     </svg>
-                    <span>Login with Steam</span>
                 </button>
             </form>
         {/if}
     </div>
 </header>
+
+<style>
+    .logo {
+        -webkit-mask: url(/logo.svg) no-repeat center;
+        mask: url(/logo.svg) no-repeat center;
+        width: 2.5rem;
+        height: 2.5em;
+        stroke: currentColor;
+    }
+</style>
